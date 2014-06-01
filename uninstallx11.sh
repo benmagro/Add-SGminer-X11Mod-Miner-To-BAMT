a778ad2dc24ff5c2ff449bde7fc1849aa52e0b1c
root <root@tango1.(none)>
2014-06-01 13:17:14 +0000

    changed exe status and moved final locationt to /etc/bamt/

--- uninstallx11.sh
+++ uninstallx11.sh
@@ -11,6 +11,7 @@ cp /tmp/common.unpatch.diff .
 sleep 2
 patch common.pl < common.unpatch.diff
 rm common.unpatch.diff
+cd /etc/bamt/
 echo 'Please remove the following lines from bamt.conf manually'
 echo '#cgminer_opts: --api-listen --config /etc/bamt/sgminer-x11mod.conf'
 echo '# Sgminer X11 Mod "DRK"'
