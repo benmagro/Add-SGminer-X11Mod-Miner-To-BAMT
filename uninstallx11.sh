d752c27ac62561bf01146e82ab07510e4134c17e
Ben Magro <bmagro@gmail.com>
2014-06-01 23:12:59 +1000

    added some cleanup

--- uninstallx11.sh
+++ uninstallx11.sh
@@ -9,7 +9,8 @@ mv sgminer-x11mod.conf sgminer-x11mod.conf.bak
 cd /opt/bamt/
 cp /tmp/common.unpatch.diff .
 sleep 2
-patch common.pl < common.unpatch.diff 
+patch common.pl < common.unpatch.diff
+rm common.unpatch.diff
 echo 'Please remove the following lines from bamt.conf manually'
 echo '#cgminer_opts: --api-listen --config /etc/bamt/sgminer-x11mod.conf'
 echo '# Sgminer X11 Mod "DRK"'
